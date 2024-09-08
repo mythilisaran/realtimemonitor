760281dc69423a7bf8532ff8a995401f85f9054a
mythilisaran <mythilisaran138@gmail.com>
2024-09-08 16:10:28 +0530

    Create BOX1.ino

---
+++ BOX1.ino
@@ -0,0 +1,73 @@
+#include <WiFi.h>
+#include "ThingSpeak.h"
+//box1
+const char* WIFI_NAME = "Wokwi-GUEST";
+const char* WIFI_PASSWORD = "";
+const int myChannelNumber = 2642618;
+const char* myApiKey = "VRM9PPSF02IOGENV";
+const char* server = "api.thingspeak.com";
+
+
+const int trigPin = 15;
+const int echoPin = 2;
+
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.println("Wifi not connected");
+  }
+  Serial.println("Wifi connected !");
+  Serial.println("Local IP: " + String(WiFi.localIP()));
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
+
+
+}
+
+void loop() {
+
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  ThingSpeak.setField(2,distanceCm);
+
+  int x = ThingSpeak.writeFields(myChannelNumber,myApiKey);
+
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+  
+  delay(1000);
+}
